ddea3b8abb07e60fc9a10053090d104ff35a2fff
Adrian Kuklinski <adriankuklinski@pm.me>
2023-12-04 00:06:58 -0500

    feat(wip): going to bed...leaving my current attempt of find_numbers_in_line method for now

--- 2023/day3/day3.ml
+++ 2023/day3/day3.ml
@@ -14,11 +14,30 @@ let read_lines filename =
 
 let is_digit c = c >= '0' && c <= '9'
 
+let find_numbers_in_line line =
+    let rec aux idx current_start current_length numbers =
+        Printf.printf "Current index: %d, Character: '%c'\n" idx (if idx < String.length line then line.[idx] else ' ');
+        if idx = String.length line then
+            if current_length > 0 then (current_start, current_length) :: numbers else numbers
+        else
+            let c = line.[idx] in
+            if is_digit c then
+                aux (idx + 1) (if current_length = 0 then idx else current_start) (current_length + 1) numbers
+            else
+                if current_length > 0 then
+                    aux (idx + 1) 0 0 ((current_start, current_length) :: numbers)
+                else
+                    aux (idx + 1) 0 0 numbers
+    in
+    List.rev (aux 0 0 0 [])
+
+[@@@warning "-26"]
 let process_lines lines =
     let rec aux prev_line current_line next_line remaining_lines =
         match current_line, next_line, remaining_lines with
         | None, _, _ -> ()
         | Some current, next, rest ->
+            let number_positions = find_numbers_in_line current in
             let next_line_str = match next with Some line -> line | None -> "None" in
             let prev_line_str = match prev_line with Some line -> line | None -> "None" in
             Printf.printf "Previous Line: %s, Current Line: %s, Next Line: %s\n" prev_line_str current next_line_str;
